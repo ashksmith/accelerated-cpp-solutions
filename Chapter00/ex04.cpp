f171f4b201a6632f90229e621b316fcce76570a7
Ashley K. Smith <ashley@ashleyksmith.com>
2017-04-10 21:39:33 +0100

    Completed ex0-4

---
+++ Chapter00/ex04.cpp
@@ -0,0 +1,16 @@
+// Author: Ashley K. Smith
+// Solution to Accelerated C++, exercise 0-4
+// "Write a program that, when run, writes the Hello World! program as it's output.
+
+#include <iostream>
+
+int main(){
+	std::cout << "#include <iostream>\n" << std::endl;
+	std::cout << "int main(){" << std::endl;
+	std::cout << "\tstd::cout << \"Hello World!\";" << std::endl;
+	std::cout << "\treturn 0;" << std::endl;
+	std::cout << "}" << std::endl;
+
+	return 0;
+}
+
