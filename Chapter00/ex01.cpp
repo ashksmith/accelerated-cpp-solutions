9339c2667888a10490e50907daf86b6b87bc252b
Ashley K. Smith <ashley@ashleyksmith.com>
2017-04-10 21:38:33 +0100

    Completed ex0-0

---
+++ Chapter00/ex01.cpp
@@ -0,0 +1,11 @@
+// Author: Ashley K. Smith
+// Accelerated C++, Chapter00, Exercise 0-0
+#include <iostream>
+
+int main(){
+	std::cout << "Hello, World" << std::endl;
+	return 0;
+}
+
+// Simple compilation:
+// 	g++ ex01.cpp
