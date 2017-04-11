b0cae8fd7f64d0e65801e3e107dabf15308bb693
Ashley K. Smith <ashley@ashleyksmith.com>
2017-04-11 19:35:30 +0100

    Completed ex0-10

---
+++ Chapter00/ex10.cpp
@@ -0,0 +1,13 @@
+// Author: Ashley K. Smith
+// Solution to Accelerated C++, exercise 0-10
+// Rewrite the Hello, World! program so that a newline occurs everywhere
+// that whitespace is allowed in the program.
+
+#include <iostream>
+
+int main(){
+	std::cout << "Hello,\nworld!" << std::endl;
+
+	return 0;
+}
+
