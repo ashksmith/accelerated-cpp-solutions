a8eafb57fea85a16aec35e2e310985359bbe3325
Ashley K. Smith <ashley@ashleyksmith.com>
2017-04-11 19:32:49 +0100

    ex09.cpp

---
+++ Chapter00/ex09.cpp
@@ -0,0 +1,8 @@
+// Author: Ashley K. Smith
+// Solution to Accelerated C++, exercise 0-9
+// "What is the shorted valid program?
+
+// This is the shortest program. Each cpp program must have a main function
+// the return statement is optional in the main function. 
+
+int main(){}
